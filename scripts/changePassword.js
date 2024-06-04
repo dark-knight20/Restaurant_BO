2dd983f6d5428185334d66fe0f64d0f3f3e1aa52
dark-knight20 <joeljosephj2011@gmail.com>
2024-06-04 22:45:01 +0530

    change password function added

--- scripts/changePassword.js
+++ scripts/changePassword.js
@@ -1,15 +1,37 @@
-function changePassword(username)
-{
-   let credentials=localStorage.getItem('credentials');
-   let newPassword=document.getElementById('').value;
-   for (user of credentials['users'])
-    {
-        if(username==user.username)
-            {
-                if(document.getElementById('').value==user.phn_number)
-                    {
-                        user.password=newPassword;
-                    }
+function changePassword() {
+    let userTest = false;
+    let credentials = localStorage.getItem('credentials');
+    let newPassword = document.getElementById('new_password').value;
+    let username = document.getElementById('username').value;
+    let phoneNumber = document.getElementById('phone_number').value;
+    for (user of credentials['users']) {
+        if (username == user.username) {
+            userTest = true;
+            if (phoneNumber == user.phone_no) {
+                user.password = newPassword;
+                localStorage.setItem('credentials', JSON.stringify(credentials));
+                alert("password changed!")
+                window.location.href = 'index.html';
             }
+            else {
+                // alert("There is no such registered phone number");
+                let phoneTag = document.getElementById('phone_number');
+                phoneTag.setCustomValidity('');
+                phoneTag.setCustomValidity('There is no such registered phone number');
+                phoneTag.reportValidity();
+            }
+            break;
+        }
+        else {
+            if (userTest==false) {
+                let userTag = document.getElementById('username');
+                userTag.setCustomValidity('');
+                userTag.setCustomValidity('wrong username!');
+                userTag.reportValidity();
+            }
+
+            // alert("wrong username");
+        }
     }
 }
+
